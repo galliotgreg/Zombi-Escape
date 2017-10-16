2c49b362d9f317eb63e376e7b75e0381d79f0749
J-Ponzo <jeremy.ponzo@gmail.com>
2017-10-16 19:41:46 +0200

    J-Ponzo : Feature Zombie Spawner
    Zombie spaner added
    ref task : ZombieSpawner

---
+++ Assets/Scripts/ZombiSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombiSpawner : MonoBehaviour {
+    [SerializeField]
+    private float frequency = 0.2f;    // spawn frequecy given on zombie / sec
+    [SerializeField]
+    private GameObject zombiePrefab = null;
+
+    private float spawnCooldown = -1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnCooldown < 0)
+        {
+            GameObject zombie = GameObject.Instantiate(zombiePrefab);
+            ZombieBehaviour zombieBehaviour = zombie.GetComponent<ZombieBehaviour>();
+            GameObject player = GameObject.FindGameObjectsWithTag("Player")[0];
+            zombieBehaviour.setTarget(player);
+
+            spawnCooldown = frequency;
+        } else
+        {
+            spawnCooldown -= Time.deltaTime;
+        }
+    }
+}
