79f1eb88ddd0f2e8b660d89ffb1820383da1fbcc
J-Ponzo <jeremy.ponzo@gmail.com>
2017-10-16 17:06:30 +0200

    J-Ponzo : Feature
    Zombie prefab :
    Simple move
    Player Prefab :
    Simple move

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+    [SerializeField]
+    private float hitRate = 1;
+    [SerializeField]
+    private float lifePoints = 100;
+    [SerializeField]
+    private float moveSpeed = 1;
+    [SerializeField]
+    private float angularSpeed = 90;
+    [SerializeField]
+    private float hitDamage = 20;
+
+    public float HitRate
+    {
+        get
+        {
+            return hitRate;
+        }
+
+        set
+        {
+            hitRate = value;
+        }
+    }
+
+    public float LifePoints
+    {
+        get
+        {
+            return lifePoints;
+        }
+
+        set
+        {
+            lifePoints = value;
+        }
+    }
+
+    public float MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+
+        set
+        {
+            moveSpeed = value;
+        }
+    }
+
+    public float AngularSpeed
+    {
+        get
+        {
+            return angularSpeed;
+        }
+
+        set
+        {
+            angularSpeed = value;
+        }
+    }
+
+    public float HitDamage
+    {
+        get
+        {
+            return hitDamage;
+        }
+
+        set
+        {
+            hitDamage = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += this.transform.right * Time.deltaTime * moveSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position -= this.transform.right * Time.deltaTime * moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0, 0, -Time.deltaTime * angularSpeed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0, 0, Time.deltaTime * angularSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieBehaviour : MonoBehaviour {
+    [SerializeField]
+    private float hitRate = 1;
+    [SerializeField]
+    private float lifePoints = 100;
+    [SerializeField]
+    private float moveSpeed = 1;
+    [SerializeField]
+    private float angularSpeed = 90;
+    [SerializeField]
+    private float hitDamage = 20;
+    [SerializeField]
+    private float aimThreshold = 2;
+
+    private long hitTimer = 0;
+
+    [SerializeField]
+    private GameObject target = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            //Manage look at
+            Vector3 trgDir = target.transform.position - this.transform.position;
+            float angle = Vector3.Angle(trgDir, this.transform.right);
+            Vector3 cross = Vector3.Cross(trgDir, this.transform.right);
+            if (cross.z < 0) { angle = -angle; }
+            if (angle > 0 && angle < 180 - aimThreshold)
+            {
+                this.transform.Rotate(0, 0, Time.deltaTime * angularSpeed);
+            }
+            if (angle < 0 && angle > -180 + aimThreshold)
+            {
+                this.transform.Rotate(0, 0, -Time.deltaTime * angularSpeed);
+            }
+
+            //manage move
+            if (Vector2.Dot(trgDir, this.transform.right) > 0)
+            {
+                this.transform.position += this.transform.right * Time.deltaTime * moveSpeed;
+            }
+        }
+    }
+}
