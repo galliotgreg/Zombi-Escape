6f866c3330ac3c918e50d3193cfcf30b1138b741
J-Ponzo <jeremy.ponzo@gmail.com>
2017-10-16 23:28:00 +0200

    J-Ponzo : Feature Poursuite Camera
    Poursuite camera implementer
    ref task : Implement Poursuite Camera

---
+++ Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour {
+    [SerializeField]
+    private GameObject trackedPlayer = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(trackedPlayer.transform.position.x, trackedPlayer.transform.position.y, this.transform.position.z);
+	}
+}
