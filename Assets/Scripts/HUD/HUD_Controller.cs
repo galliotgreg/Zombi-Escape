8fa8c8664a778625afef02775dac34bb65a3640c
edwynluis <edwynluis@yahoo.com.br>
2017-10-17 17:15:19 +0200

    Initial HUD
    scratch from a HUD showing the plater's life points

---
+++ Assets/Scripts/HUD/HUD_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Controller : MonoBehaviour {
+
+	[SerializeField]
+	private PlayerModel player = null;
+	[SerializeField]
+	private Text playerLife = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerLife.text = "Life: "+player.LifePoints.ToString();
+	}
+}
