abd743233eb0e5ca0e0bc1a2c76ed44bfd34dca1
J-Ponzo <jeremy.ponzo@gmail.com>
2017-10-21 11:00:22 +0200

    J-Ponzo : Add Exit Prefab
    Implementation du point d extraction

---
+++ Assets/Scripts/ExtractPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtractPoint : MonoBehaviour {
+
+    private int nbPlayersInRange = 0;
+    private float range = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updatePlayersInRange();
+
+		if (this.nbPlayersInRange == GameState.instance.NbPlayers)
+        {
+            Debug.Log("Victory!");
+        }
+	}
+
+    private void updatePlayersInRange()
+    {
+        int playersInRange = 0;
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            if (Vector3.Distance(this.transform.position, player.transform.position) < this.range)
+            {
+                playersInRange++;
+            }
+        }
+
+        this.nbPlayersInRange = playersInRange;
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+    /// <summary>
+    /// The static instance of the Singleton for external access
+    /// </summary>
+    public static GameState instance = null;
+
+    /// <summary>
+    /// Enforce Singleton properties
+    /// </summary>
+    void Awake()
+    {
+        //Check if instance already exists and set it to this if not
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        //Enforce the unicity of the Singleton
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    [SerializeField]
+    private int nbPlayers = 1;
+
+    public int NbPlayers
+    {
+        get
+        {
+            return nbPlayers;
+        }
+
+        set
+        {
+            nbPlayers = value;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
