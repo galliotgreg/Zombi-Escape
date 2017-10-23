d346ed0b3221dd4f75bd1c440109111f98831cd8
J-Ponzo <jeremy.ponzo@gmail.com>
2017-10-23 16:36:23 +0200

    J-Ponzo : Shadow Casting
    Add custom shader for sprite shadowCasting

---
+++ Assets/Scripts/Utills/ReceiveShadows.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveShadows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        GetComponent<Renderer>().receiveShadows = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
